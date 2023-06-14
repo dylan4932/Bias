0f98157f0e41c447dc3e53382da4d050c69723ee
“dylan4932” <dylan4932@gmail.com>
2023-06-14 02:09:37 -0400

    v1.0 updates
    portal-text style edit
    change mongoDB Cluster

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const mongoURI = process.env.MONGODB_URI || 
-'mongodb+srv://team40:123@cluster1.trpht.mongodb.net/BiasTree?retryWrites=true&w=majority'
+'mongodb+srv://onion-portal:Mmwi0GJw6PMXEevG@portal-cluster.ubjjkhb.mongodb.net/onion-portal?retryWrites=true&w=majority'
 
 mongoose.set("strictQuery", false);
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
